0466a004e6e5214f9e365f587bdb0c1b27f5e7f5
ahm28 <ahmad.mughni1905@gmail.com>
2022-06-07 18:38:58 +0700

    add index.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import AppBarComponent from "./components/AppBar/AppBarComponent";
 
 function App() {
   return (
     <div className="App">
       <p>
+        <AppBarComponent />
         Edit <code>src/App.js</code> and save to reload.
       </p>
     </div>
